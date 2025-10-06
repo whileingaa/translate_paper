30b7952c7709d12d4e84eb3fe1738c455de5c5c0
whileingaa <whileingaa@gmail.com>
2025-10-06 12:10:10 +0800

    在main函数中继承了主要模块功能

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from mineru_ocr import request_mineru_translate
 from concurrent_translate import translate_paper
-def main():
+def main(input_file:str, output_file :str="./output", extract_zip_after=True, delete_zip=True,max_tokens=2048, max_workers=3, max_retries=3):
     # 示例用法
-    test_filepath = r"C:\Users\zzz\Zotero\storage\QS95IVS6\Park 等 - 2023 - Generative Agents Interactive Simulacra of Human Behavior.pdf"
-    input_md_path = request_mineru_translate(test_filepath, output_dir="./output", extract_zip_after=True, delete_zip=True)
+
+    input_md_path = request_mineru_translate(filepath=input_file, output_dir=output_file, extract_zip_after=extract_zip_after, delete_zip=delete_zip)
     if not input_md_path:
         print("OCR 处理失败，无法继续翻译")
         return
@@ -13,11 +13,20 @@ def main():
     translate_paper(
         input_md_path=input_md_path,
         output_md_path=None,  # None 表示自动生成输出路径
-        max_tokens=2048,   # 每块最大 token 数
-        max_workers=5,     # 并发线程数
-        max_retries=3      # 重试次数
+        max_tokens=max_tokens,   # 每块最大 token 数
+        max_workers=max_workers,     # 并发线程数
+        max_retries=max_retries      # 重试次数
     )
 
 if __name__ == "__main__":
-    main()
+    test_filepath = r"C:\Users\zzz\Zotero\storage\QS95IVS6\Park 等 - 2023 - Generative Agents Interactive Simulacra of Human Behavior.pdf"
+    main(
+        input_file=test_filepath,
+        output_file="./output",
+        extract_zip_after=True,
+        delete_zip=True,
+        max_tokens=2048,
+        max_workers=5,
+        max_retries=3
+    )
     
